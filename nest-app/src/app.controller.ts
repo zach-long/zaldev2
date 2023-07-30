10551e92bdbbf76890b4673fe80bc84e74dcef5a
Zachary Long <zach.writes.code@gmail.com>
2023-07-30 15:42:18 -0400

    establish admin routes; create user resource

--- nest-app/src/app.controller.ts
+++ nest-app/src/app.controller.ts
@@ -1,27 +1,24 @@
-import { Get, Controller, Render } from '@nestjs/common';
+import { Get, Controller, Res, Render } from '@nestjs/common';
+import { Response } from 'express';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-    @Get()
-    @Render('index')
-    root() {
-        return { pageTitle: 'Zachary Long - Full Stack Developer' };
-    }
+  constructor(private appService: AppService) {}
+
+  @Get()
+  renderRoot(@Res() res: Response) {
+    return res.render(
+      'index',
+      { pageTitle: 'Zachary Long - Full Stack Developer' },
+    );
+  }
+
+  @Get('admin')
+  renderAdmin(@Res() res: Response) {
+    return res.render(
+        'admin/index',
+        { pageTitle: 'Admin' }
+    );
+  }
 }
-
-// import { Get, Controller, Res, Render } from '@nestjs/common';
-// import { Response } from 'express';
-// import { AppService } from './app.service';
-
-// @Controller()
-// export class AppController {
-//   constructor(private appService: AppService) {}
-
-//   @Get()
-//   root(@Res() res: Response) {
-//     return res.render(
-//       this.appService.getViewName(),
-//       { message: 'Hello world!' },
-//     );
-//   }
-// }
--- nest-app/src/app.module.ts
+++ nest-app/src/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import configuration from './config/configuration';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({load: [configuration]})],
+  imports: [ConfigModule.forRoot({load: [configuration]}), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
